a5ac830fade78ef2d62cb4f4c9791789b7b7dc5a
mishu <mishu@xtelecom.ro>
2018-01-14 11:20:43 +0200

    adds time limit for tests that need the database

--- src/app/Traits/SignIn.php
+++ src/app/Traits/SignIn.php
@@ -6,6 +6,8 @@ trait SignIn
 {
     protected function signIn($user = null)
     {
+        set_time_limit(30);
+
         $user = $user ?: factory('App\User')->create();
 
         $this->actingAs($user);
